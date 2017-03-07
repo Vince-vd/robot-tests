c259509982779b76d8ec996e93f3c61b47fb57a3
VincentVandyck <vincent.vandyck@gmail.com>
2017-03-07 13:49:53 -0500

    changed .h

--- src/pMotorCommand/MotorCommand.h
+++ src/pMotorCommand/MotorCommand.h
@@ -33,10 +33,10 @@ class MotorCommand : public CMOOSApp
 
    double m_speed;
    double m_command;
-   // int m_I2C;
-   // int m_num;
-   // adafruit_motorhat m_mH;
-   // adafruit_dc_motor m_motor;
+   int m_I2C;
+   int m_num;
+   adafruit_motorhat m_mH;
+   adafruit_dc_motor m_motor;
 
  private: // Configuration variables
 
